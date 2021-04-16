a4a0812a8f206e67ce2c08123dae40e0269739ff
shayan_jaleel <shayan.jaleel@gmail.com>
2021-04-16 04:11:16 -0400

    Change schema, model and controller for quizzes and questions to link to db using Mongoose.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // TODO: review https://expressjs.com/
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/whiteboard',
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
 // configure CORS
 app.use(function (req, res, next) {
