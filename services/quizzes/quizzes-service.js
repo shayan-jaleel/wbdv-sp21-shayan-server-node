4888381fd8047cd812855714728015cf710221bb
shayan_jaleel <shayan.jaleel@gmail.com>
2021-04-16 13:46:31 -0400

    Refactor quizzes service to use quizzes dao.

--- services/quizzes/quizzes-service.js
+++ services/quizzes/quizzes-service.js
@@ -1,18 +1,22 @@
 
 const quizzes = require("./quizzes.json")
 
-const quizzesModel = require("../../db/quizzes/quizzes-model")
+const quizzesDao = require("../../daos/quizzes-dao")
+// const quizzesModel = require("../../db/quizzes/quizzes-model")
 
 const createQuiz = () => {}
 const findAllQuizzes = () => {
-    return quizzesModel.find()
+    return quizzesDao.findAllQuizzes()
+    // return quizzesModel.find()
     // return quizzes
 }
 const findQuizById = (qid) => {
-    return quizzesModel
-        .findById(qid)
-        .populate("questions")
-        .exec()
+    return quizzesDao.findQuizById(qid)
+    // return quizzesModel
+    //     .findById(qid)
+    //     .populate("questions")
+    //     .exec()
+
     // return quizzes.find((quiz) => {
     //     return (quiz._id === qid)
     // })
